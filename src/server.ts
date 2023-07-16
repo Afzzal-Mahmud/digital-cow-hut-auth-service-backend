fcbdf590038f3c5310b57155d06a18780dc725de
afzal mahmud <afzzalmahmud@gmail.com>
2023-07-16 23:58:06 +0600

    [Done] server created with proper err handling manner

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+import { Server } from 'http'
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
+import { errorLogger, successLogger } from './shared/logger'
+
+let server: Server
+
+async function shutdown() {
+  try {
+    if (server) {
+      await new Promise<void>((resolve, reject) => {
+        server.close(err => {
+          if (err) {
+            errorLogger.error(`Error while closing server: ${err}`)
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      })
+    }
+
+    await mongoose.disconnect()
+    successLogger.info('Server gracefully shut down.')
+    process.exit(0)
+  } catch (error) {
+    errorLogger.error(`Error during server shutdown: ${error}`)
+    process.exit(1)
+  }
+}
+
+process.on('SIGINT', () => {
+  successLogger.info('SIGINT is received')
+  shutdown()
+})
+
+process.on('SIGTERM', () => {
+  successLogger.info('SIGTERM is received')
+  shutdown()
+})
+
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  successLogger.info('uncaughtException is received')
+  shutdown()
+})
+
+process.on('unhandledRejection', error => {
+  errorLogger.error(error)
+  successLogger.info('unhandledRejection is received')
+  shutdown()
+})
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    successLogger.info('Database is connected successfully')
+
+    server = app.listen(config.port, () => {
+      successLogger.info(
+        `Digital CowHut Application listening on port ${config.port}`
+      )
+    })
+  } catch (error) {
+    errorLogger.error('Failed to connect to the database', error)
+  }
+}
+
+main()
