1c98e9b2d76ea99422b3a7a863c3a6d46f691531
afzal mahmud <afzzalmahmud@gmail.com>
2023-07-19 11:13:47 +0600

    [Done] err,api interface,async,and payload success handler created

---
+++ src/interfaces/IApiResponseOnPayloadSuccess.ts
@@ -0,0 +1,13 @@
+type IMetaType = {
+  page: number
+  limit: number
+  total: number
+}
+
+export type IApiResponseOnPayloadSuccess<T> = {
+  statusCode: number
+  success: boolean
+  message?: string | null | undefined
+  meta?: IMetaType | null | undefined
+  data: T | null
+}
---
+++ src/interfaces/genericErrorMessage.interface.ts
@@ -0,0 +1,5 @@
+export type IGenericErrorMessages = {
+  path: string | number
+  message: string
+  stack?: string
+}
